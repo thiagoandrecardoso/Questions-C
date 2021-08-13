c89ce37b519982660dca99cb9748112f85734fe3
Thiago André Cardoso Silva <thiagoandrecardoso@icloud.com>
2021-08-13 10:00:39 -0300

    Versão 2 do Fibonacci

--- IFF/fibonacci.c
+++ IFF/fibonacci.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int fibonacci_n_esimo_termo(int n, int n1, int n2);
+int fib(int n, int n1, int n2)
+{
+    int auxiliar;
+    if (n == 1)
+        return n1;
+    if (n == 2)
+        return n2;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        auxiliar = n1 +  n2;
+        n1 = n2;
+        n2 = auxiliar;
+    }
+    return n1;
+}
 
 int main(int argc, char const *argv[])
 {
@@ -13,22 +28,9 @@ int main(int argc, char const *argv[])
     scanf("%d", &n1);
     printf("Digite o segundo termo\n");
     scanf("%d", &n2);
-    printf("O %d numero da sequencia de fibonacci = %d\n\n", n, fibonacci_n_esimo_termo(n, n1, n2));
+    printf("O %d numero da sequencia de fibonacci = %d\n\n", n, fib(n, n1, n2));
     return 0;
 }
 
-int fibonacci_n_esimo_termo(int n, int n1, int n2)
-{
-    if (n == 1)
-        return n1;
-    if (n == 2)
-        return n2;
 
-    for (int i = 0; i < n - 1; i++)
-    {
-        n2 += n1;
-        n1 = n2 - n1;
-    }
-    return n1;
-}
 
