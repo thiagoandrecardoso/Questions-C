81f723d578f809f741519698dc5037da27f611c5
Thiago André Cardoso Silva <thiagoandrecardoso@icloud.com>
2021-08-13 09:55:54 -0300

    Versão 2 do vetor aleatorio

--- IFF/random_vector.c
+++ IFF/random_vector.c
@@ -1,65 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<time.h>
+#include <time.h>
 
-int maior_elemento(int v[], int len);
-int menor_elemento(int v[], int len);
-int quantidade_pares(int v[], int len);
-
-int main(int argc, char const *argv[])
-{
-    const int LEN = 10000;
-    srand(time(NULL));
-    int v[LEN];
-
-    for (int i = 0; i < LEN; i++)
-    {
-        v[i] = rand() % 10000;
-    }
-
-    printf("Maior valor = %d\n", maior_elemento(v, LEN));
-    printf("Menor valor = %d\n", menor_elemento(v, LEN));
-    int q_pares = quantidade_pares(v, LEN);
-    int q_impares = LEN - q_pares;
-
-    printf("Quantidade de pares = %d\n", q_pares);
-    printf("Quantidade de impares = %d\n", q_impares);
-
-    return 0;
-}
-
-int maior_elemento(int v[], int len)
+int maior(int v[], int tamanho)
 {
     int maior = v[0];
 
-    for (int i = 1; i < len; i++)
+    for (int i = 1; i < tamanho; i++)
     {
-        if (v[i] > maior)
+        if (v[i] > maior){
             maior = v[i];
+        }
     }
     return maior;
 }
 
-int menor_elemento(int v[], int len)
+int menor(int v[], int tamanho)
 {
     int menor = v[0];
 
-    for (int i = 1; i < len; i++)
+    for (int i = 1; i < tamanho; i++)
     {
         if (v[i] < menor)
+        {
             menor = v[i];
+        }
     }
     return menor;
 }
 
-int quantidade_pares(int v[], int len)
+int retorna_qntd_pares(int v[], int tamanho)
 {
     int q = 0;
-    for (int i = 1; i < len; i++){
+    for (int i = 1; i < tamanho; i++)
+    {
         if (v[i] % 2 == 0)
         {
             q++;
         }
     }
     return q;
+}
+
+int retorna_qntd_impares(int v[], int tamanho)
+{
+    int q = 0;
+    for (int i = 1; i < tamanho; i++)
+    {
+        if (v[i] % 2 != 0)
+        {
+            q++;
+        }
+    }
+    return q;
+}
+
+int main()
+{
+    srand(time(NULL));
+    int vetor[10000];
+
+    for (int i = 0; i < 10000; i++)
+    {
+        vetor[i] = rand() % 10000;
+    }
+
+    printf("Maior valor = %d\n", maior(vetor, 10000));
+    printf("Menor valor = %d\n", menor(vetor, 10000));
+
+    printf("Quantidade de pares = %d\n", retorna_qntd_pares(vetor, 10000));
+    printf("Quantidade de impares = %d\n", retorna_qntd_impares(vetor, 10000));
+
+    return 0;
 }
